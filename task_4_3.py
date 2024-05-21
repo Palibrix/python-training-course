64fb4dc687dd6e0d4e59cf5b684e05bf992cf10f
palibrix <palibrix@gmail.com>
2024-05-21 20:13:31 +0300

    Fix 4.3

--- task_4_3.py
+++ task_4_3.py
@@ -17,4 +17,5 @@
 інакше перевіряти результат.
 """
 config = "switchport trunk allowed vlan 1,3,10,20,30,100"
-print(config.split(' ')[-1].split(','))
+result = config.split(' ')[-1].split(',')
+print(result)
